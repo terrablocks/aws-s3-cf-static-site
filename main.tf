7aecb2a6539a736b26e27c4060b19576d2f4bb7d
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2020-01-18 13:45:54 +0530

    Upgrade to terraform v0.12 syntax

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
-  acl = "private"
+  acl    = "private"
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
@@ -14,23 +14,23 @@ locals {
 resource "aws_cloudfront_distribution" "website_cdn" {
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id = local.s3_origin_id
+    origin_id   = local.s3_origin_id
     origin_path = var.origin_path
 
     s3_origin_config {
       origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
     }
   }
-  
-  enabled = true
+
+  enabled             = true
   default_root_object = "index.html"
-  aliases = var.cnames
+  aliases             = var.cnames
 
   default_cache_behavior {
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-    compress = true
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_origin_id
+    compress               = true
     viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
@@ -42,9 +42,9 @@ resource "aws_cloudfront_distribution" "website_cdn" {
 
   price_class = "PriceClass_All"
   viewer_certificate {
-    acm_certificate_arn = length(var.cnames) == 0 ? null : aws_acm_certificate_validation.cert_validation.certificate_arn
+    acm_certificate_arn            = length(var.cnames) == 0 ? null : aws_acm_certificate_validation.cert_validation.certificate_arn
     cloudfront_default_certificate = length(var.cnames) == 0 ? true : false
-    ssl_support_method = "sni-only"
+    ssl_support_method             = "sni-only"
   }
 
   restrictions {
@@ -75,18 +75,18 @@ POLICY
 }
 
 provider "aws" {
-  alias = "us"
-  profile = var.profile
+  alias      = "us"
+  profile    = var.profile
   access_key = var.access_key
   secret_key = var.secret_key
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
 resource "aws_acm_certificate" "cert" {
-  provider = aws.us
-  domain_name = element(slice(var.cnames, 0, 1), 0)
+  provider                  = aws.us
+  domain_name               = element(slice(var.cnames, 0, 1), 0)
   subject_alternative_names = length(var.cnames) > 1 ? slice(var.cnames, 1, length(var.cnames)) : null
-  validation_method = "DNS"
+  validation_method         = "DNS"
 }
 
 data "aws_route53_zone" "zone" {
@@ -94,7 +94,7 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "cert_record" {
-  count = length(var.cnames)
+  count   = length(var.cnames)
   name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
   type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
   zone_id = data.aws_route53_zone.zone.id
@@ -103,20 +103,20 @@ resource "aws_route53_record" "cert_record" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.us
+  provider                = aws.us
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = aws_route53_record.cert_record.*.fqdn
 }
 
 resource "aws_route53_record" "website-record" {
-  count = length(var.cnames)
+  count   = length(var.cnames)
   zone_id = data.aws_route53_zone.zone.id
   name    = var.cnames[count.index]
   type    = "A"
-  
+
   alias {
-    name = aws_cloudfront_distribution.website_cdn.domain_name
-    zone_id = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "s3_bucket" {
-  value = "${aws_s3_bucket.website_bucket.id}"
+  value = aws_s3_bucket.website_bucket.id
 }
 
 output "cloudfront_endpoint" {
-  value = "${aws_cloudfront_distribution.website_cdn.domain_name}"
+  value = aws_cloudfront_distribution.website_cdn.domain_name
 }
 
 output "website_endpoint" {
-  value = "${aws_route53_record.website-record.*.fqdn}"
+  value = aws_route53_record.website-record.*.fqdn
 }
