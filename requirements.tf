039ca906158886972052b2dd0f39c33f6b7b1c56
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2021-05-03 18:38:34 +0530

    disable configuration_aliases option

--- requirements.tf
+++ requirements.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 3.37.0"
-      configuration_aliases = [aws.us]
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+      # configuration_aliases = [aws.us]  # bug: https://github.com/hashicorp/terraform/issues/28490
     }
     random = {
       source  = "hashicorp/random"
