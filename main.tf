017be13e8cece535daecb32e9aca92f0a8929eca
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2022-06-22 19:24:47 +0530

    switch back to earlier naming convention for resources

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-data "aws_kms_key" "website" {
+data "aws_kms_key" "website_bucket" {
   key_id = var.kms_key
 }
 
-resource "aws_s3_bucket" "website" {
+resource "aws_s3_bucket" "website_bucket" {
   # checkov:skip=CKV_AWS_18: Access logging not required on bucket as the same can be enabled at CloudFront level if required by user
   # checkov:skip=CKV_AWS_144: CRR not required
   # checkov:skip=CKV_AWS_21: Versioning not required
@@ -14,46 +14,46 @@ resource "aws_s3_bucket" "website" {
   tags          = var.tags
 }
 
-resource "aws_s3_bucket_acl" "website" {
-  bucket = aws_s3_bucket.website.id
+resource "aws_s3_bucket_acl" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "website" {
-  bucket = aws_s3_bucket.website.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = var.kms_key == "alias/aws/s3" ? "AES256" : "aws:kms"
-      kms_master_key_id = var.kms_key == "alias/aws/s3" ? null : data.aws_kms_key.website.id
+      kms_master_key_id = var.kms_key == "alias/aws/s3" ? null : data.aws_kms_key.website_bucket.id
     }
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "website" {
-  bucket                  = aws_s3_bucket.website.id
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket                  = aws_s3_bucket.website_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_cloudfront_origin_access_identity" "website" {
-  comment = aws_s3_bucket.website.id
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = aws_s3_bucket.website_bucket.id
 }
 
 locals {
   s3_origin_id = md5(var.bucket_name)
 }
 
-resource "aws_cloudfront_distribution" "website" {
+resource "aws_cloudfront_distribution" "website_cdn" {
   origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
     origin_path = var.origin_path
 
     s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.website.id}"
+      origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
     }
   }
 
@@ -99,7 +99,7 @@ resource "aws_cloudfront_distribution" "website" {
 
   viewer_certificate {
     cloudfront_default_certificate = length(var.cnames) == 0 ? true : false
-    acm_certificate_arn            = length(var.cnames) == 0 ? null : aws_acm_certificate_validation.website.certificate_arn
+    acm_certificate_arn            = length(var.cnames) == 0 ? null : aws_acm_certificate_validation.cert_validation.certificate_arn
     ssl_support_method             = length(var.cnames) == 0 ? null : var.ssl_support_method
     minimum_protocol_version       = length(var.cnames) == 0 ? null : var.ssl_cert_protocol_version
   }
@@ -134,7 +134,7 @@ resource "aws_cloudfront_distribution" "website" {
   tags = var.tags
 }
 
-data "aws_iam_policy_document" "website" {
+data "aws_iam_policy_document" "website_bucket_policy" {
   statement {
     actions = [
       "s3:GetObject",
@@ -142,13 +142,13 @@ data "aws_iam_policy_document" "website" {
     ]
 
     resources = [
-      aws_s3_bucket.website.arn,
-      "${aws_s3_bucket.website.arn}/*"
+      aws_s3_bucket.website_bucket.arn,
+      "${aws_s3_bucket.website_bucket.arn}/*"
     ]
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.website.id}"]
+      identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.origin_access_identity.id}"]
     }
   }
 
@@ -157,8 +157,8 @@ data "aws_iam_policy_document" "website" {
     effect  = "Deny"
     actions = ["s3:*"]
     resources = [
-      aws_s3_bucket.website.arn,
-      "${aws_s3_bucket.website.arn}/*"
+      aws_s3_bucket.website_bucket.arn,
+      "${aws_s3_bucket.website_bucket.arn}/*"
     ]
     principals {
       type        = "*"
@@ -172,16 +172,16 @@ data "aws_iam_policy_document" "website" {
   }
 }
 
-resource "aws_s3_bucket_policy" "website" {
-  bucket = aws_s3_bucket.website.id
-  policy = var.bucket_policy == "" ? data.aws_iam_policy_document.website.json : var.bucket_policy
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = var.bucket_policy == "" ? data.aws_iam_policy_document.website_bucket_policy.json : var.bucket_policy
 }
 
 provider "aws" {
   alias = "us"
 }
 
-resource "aws_acm_certificate" "website" {
+resource "aws_acm_certificate" "cert" {
   provider                  = aws.us
   domain_name               = element(slice(var.cnames, 0, 1), 0)
   subject_alternative_names = length(var.cnames) > 1 ? slice(var.cnames, 1, length(var.cnames)) : null
@@ -195,7 +195,7 @@ data "aws_route53_zone" "zone" {
 resource "aws_route53_record" "cert_record" {
   provider = aws.us
   for_each = {
-    for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -210,9 +210,9 @@ resource "aws_route53_record" "cert_record" {
   ttl             = 60
 }
 
-resource "aws_acm_certificate_validation" "website" {
+resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = aws.us
-  certificate_arn         = aws_acm_certificate.website.arn
+  certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_record : record.fqdn]
 }
 
@@ -220,7 +220,7 @@ locals {
   website_domains = length(var.website_domains) == 0 ? var.cnames : var.website_domains
 }
 
-resource "aws_route53_record" "website" {
+resource "aws_route53_record" "website-record" {
   count           = length(local.website_domains)
   allow_overwrite = true
   zone_id         = data.aws_route53_zone.zone.id
@@ -228,8 +228,8 @@ resource "aws_route53_record" "website" {
   type            = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.website.domain_name
-    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
--- output.tf
+++ output.tf
@@ -1,25 +1,25 @@
 output "s3_bucket" {
-  value       = aws_s3_bucket.website.id
+  value       = aws_s3_bucket.website_bucket.id
   description = "ID/Name of S3 bucket created for serving static website"
 }
 
 output "s3_bucket_arn" {
-  value       = aws_s3_bucket.website.arn
+  value       = aws_s3_bucket.website_bucket.arn
   description = "ARN of S3 bucket created for serving static website"
 }
 
 output "cloudfront_id" {
-  value       = aws_cloudfront_distribution.website.id
+  value       = aws_cloudfront_distribution.website_cdn.id
   description = "ID of CloudFront distribution created for serving static website"
 }
 
 output "cloudfront_arn" {
-  value       = aws_cloudfront_distribution.website.arn
+  value       = aws_cloudfront_distribution.website_cdn.arn
   description = "ARN of CloudFront distribution created for serving static website"
 }
 
 output "cloudfront_endpoint" {
-  value       = aws_cloudfront_distribution.website.domain_name
+  value       = aws_cloudfront_distribution.website_cdn.domain_name
   description = "Endpoint of CloudFront distribution created for serving static website"
 }
 
